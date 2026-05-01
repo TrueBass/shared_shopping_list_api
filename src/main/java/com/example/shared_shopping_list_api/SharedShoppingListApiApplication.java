510a64c314005e0b75e17ad4bb82ba8709835f8f
TrueBass <ttrrbb81@gmail.com>
2026-05-01 09:23:05 +0200

    Initial commit

---
+++ src/main/java/com/example/shared_shopping_list_api/SharedShoppingListApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shared_shopping_list_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedShoppingListApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SharedShoppingListApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/shared_shopping_list_api/SharedShoppingListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shared_shopping_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedShoppingListApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
